ac5ad31a0bec1310b39671b4443c59bbf5b3781a
grantkeddy <grant.keddy@gmail.com>
2026-06-12 15:51:56 -0300

    Update firebase.js

--- src/firebase.js
+++ src/firebase.js
@@ -1,1 +1,16 @@
-export const db = null;
+import { initializeApp } from "firebase/app";
+import { getDatabase } from "firebase/database";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyCx9UJI7ktgrC-1M-gx3uQNIrPIUUhbgbo",
+  authDomain: "solenne-and-grant.firebaseapp.com",
+  databaseURL: "https://solenne-and-grant-default-rtdb.firebaseio.com",
+  projectId: "solenne-and-grant",
+  storageBucket: "solenne-and-grant.firebasestorage.app",
+  messagingSenderId: "150856047043",
+  appId: "1:150856047043:web:d7eb85974388a671fdc576",
+};
+
+const app = initializeApp(firebaseConfig);
+
+export const db = getDatabase(app);
